ff7f9305041c77d03a0d6f48727d7e9cbcf5e60a
Max <67809373+98ping@users.noreply.github.com>
2023-12-03 12:39:03 -0800

    feat: day3 is hard

--- src/day3/Day03.kt
+++ src/day3/Day03.kt
@@ -2,6 +2,7 @@ package day3
 
 import readInput
 
+// gave up on this one
 fun main()
 {
     val start = System.currentTimeMillis()
@@ -12,8 +13,11 @@ fun main()
 
 fun part1()
 {
-    val testInput = readInput("day3", "test")
+    val testInput = readInput("day3", "input")
     val validNumbers = mutableListOf<Int>()
+    val occupiedIndices = mutableMapOf<Int, MutableList<Int>>()
+    var sum = 0
+
 
     for (i in testInput.indices)
     {
@@ -24,15 +28,41 @@ fun part1()
         {
             val indices = findIndicesFor(number, line)
 
-            for (index in indices) {
-                println("Index of number: $index")
+            for (index in indices)
+            {
+                val top = testInput.getOrNull(i - 1)
+                val bottom = testInput.getOrNull(i + 1)
+
+                if (hasSymbolNearby(index, line, top, bottom))
+                {
+                    val occupied = occupiedIndices.getOrDefault(i, mutableListOf())
+
+                    // means there is duplicate
+                    if (indices == occupied) {
+                        validNumbers.add(number)
+                        break
+                    }
+
+                    if (!occupied.contains(index))
+                    {
+                        validNumbers.add(number)
+
+                        occupied.addAll(indices)
+                        occupiedIndices[i] = occupied
+                        break
+                    }
+                }
             }
         }
     }
 
-    for (number in validNumbers) {
+    for (number in validNumbers)
+    {
+        sum += number
         println("Found valid number: $number")
     }
+
+    println("Sum is: $sum")
 }
 
 fun getNumbers(line: String): MutableList<Int>
@@ -45,7 +75,7 @@ fun getNumbers(line: String): MutableList<Int>
     {
         val char = line[i]
 
-        if (char.isDigit())
+        if (char.isDigit() || char == '-')
         {
             if (stillDigit)
             {
@@ -59,7 +89,14 @@ fun getNumbers(line: String): MutableList<Int>
         {
             if (currentNumber.isNotEmpty())
             {
-                numbers.add(currentNumber.joinToString("").toInt())
+                val number = currentNumber.joinToString("").toIntOrNull()
+
+                // just in case our test case is like
+                // -------
+                if (number != null)
+                {
+                    numbers.add(number)
+                }
             }
 
             stillDigit = false
@@ -70,41 +107,75 @@ fun getNumbers(line: String): MutableList<Int>
     return numbers
 }
 
+fun hasSymbolNearby(index: Int, currentLine: String, topLine: String?, bottomLine: String?): Boolean
+{
+    val next = currentLine.getOrNull(index + 1)
+    val prev = currentLine.getOrNull(index + 1)
 
-fun findIndicesFor(number: Int, l: String) : MutableList<Int> {
+
+    if (next != null && isSymbol(next)) return true
+    if (prev != null && isSymbol(prev)) return true
+
+    if (topLine != null)
+    {
+        val topCenter = topLine[index]
+        val topLeft = topLine.getOrNull(index - 1)
+        val topRight = topLine.getOrNull(index + 1)
+
+
+        if (isSymbol(topCenter) || isSymbol(topLeft) || isSymbol(topRight))
+        {
+            return true
+        }
+    }
+
+    if (bottomLine != null)
+    {
+        val bottomCenter = bottomLine[index]
+        val bottomLeft = bottomLine.getOrNull(index - 1)
+        val bottomRight = bottomLine.getOrNull(index + 1)
+
+        if (isSymbol(bottomCenter) || isSymbol(bottomLeft) || isSymbol(bottomRight))
+        {
+            return true
+        }
+    }
+
+    return false
+}
+
+
+fun findIndicesFor(number: Int, l: String): MutableList<Int>
+{
     val indices = mutableListOf<Int>()
     val numberString = number.toString()
     var line = l
+    var removedCharacters = 0
 
     if (!line.contains(numberString)) return indices
 
-    while (line.contains(numberString)) {
-        val index = line.indexOf(numberString)
-        val end = index + (numberString.length - 1)
+    while (line.contains(numberString))
+    {
+        val index = line.indexOf(numberString) + removedCharacters
+        val end = index + (numberString.length)
         indices.add(index)
 
-        for (int in 1 until numberString.length) {
+        for (int in 1 until numberString.length)
+        {
             indices.add(index + int)
         }
 
-        line = line.substring(0, end)
+        line = try
+        {
+            line.removeRange(0, end)
+        } catch (e: NegativeArraySizeException) {
+            line.removeRange(0, line.length)
+        }
+
+        removedCharacters = end
     }
 
     return indices
 }
 
-fun getSymbolsInString(string: String): MutableList<Int>
-{
-    val symbols = mutableListOf<Int>()
-
-    for (i in string.indices)
-    {
-        val c = string[i]
-        if (!c.isDigit() && c != '.')
-        {
-            symbols.add(i)
-        }
-    }
-
-    return symbols
-}
+fun isSymbol(c: Char?): Boolean = c != null && !c.isDigit() && c != '-' && c != '.'
