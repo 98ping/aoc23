a8c3c7a8f650babaa1e2cfb23bc9b583af4026cc
Max <67809373+98ping@users.noreply.github.com>
2023-12-04 14:19:48 -0800

    feat: day4 done

--- src/day4/Day04.kt
+++ src/day4/Day04.kt
@@ -15,7 +15,8 @@ fun part1()
     val testInput = readInput("day4", "test")
     var globalWorth = 0
 
-    for (card in testInput) {
+    for (card in testInput)
+    {
         val game = card.removeRange(0, 9)
         val winningNumbers = game.substring(0, 30)
         val winningList = mutableListOf<Int>()
@@ -27,7 +28,8 @@ fun part1()
 
         println("Winning numbers are: " + winningNumbers.split(" "))
 
-        for (number in winningNumbers.split(" ")) {
+        for (number in winningNumbers.split(" "))
+        {
             var parsed = number
             if (number.startsWith(" ")) parsed = number.removePrefix(" ")
 
@@ -40,17 +42,21 @@ fun part1()
 
         println("Game: " + game.removeRange(0, 32))
 
-        for (gameNumber in ourGame.split(" ")) {
+        for (gameNumber in ourGame.split(" "))
+        {
             var parsed = gameNumber
             if (gameNumber.startsWith(" ")) parsed = gameNumber.removePrefix(" ")
 
             val integer = parsed.toIntOrNull() ?: continue
 
-            if (winningList.contains(integer)) {
-                if (matches == 0) {
+            if (winningList.contains(integer))
+            {
+                if (matches == 0)
+                {
                     worth = 1
                     matches = 1
-                } else {
+                } else
+                {
                     worth = (worth * 2)
                     matches++
                 }
@@ -69,20 +75,20 @@ fun part1()
 // playing area range [0, 32]
 fun part2()
 {
-    val testInput = readInput("day4", "test")
-    var totalCards = 0
+    val testInput = readInput("day4", "input")
+    val listOfCards = mutableListOf<Card>()
 
-    for (card in testInput) {
-        val game = card.removeRange(0, 7)
-        val winningNumbers = game.substring(0, 16)
+    for (card in testInput)
+    {
+        val game = card.removeRange(0, 9)
+        val winningNumbers = game.substring(0, 30)
         val winningList = mutableListOf<Int>()
+        val ourNumbers = mutableListOf<Int>()
 
-        val gameId = card.substring(4, 6).trim()
+        val gameId = card.substring(4, 8).trim()
 
-        var worth = 0
-        var matches = 0
-
-        for (number in winningNumbers.split(" ")) {
+        for (number in winningNumbers.split(" "))
+        {
             var parsed = number
             if (number.startsWith(" ")) parsed = number.removePrefix(" ")
 
@@ -91,30 +97,71 @@ fun part2()
             winningList.add(integer)
         }
 
-        val ourGame = game.removeRange(0, 16)
+        val ourGame = game.removeRange(0, 32)
 
-        for (gameNumber in ourGame.split(" ")) {
+        for (gameNumber in ourGame.split(" "))
+        {
             var parsed = gameNumber
             if (gameNumber.startsWith(" ")) parsed = gameNumber.removePrefix(" ")
 
             val integer = parsed.toIntOrNull() ?: continue
 
-            if (winningList.contains(integer)) {
-                if (matches == 0) {
-                    worth = 1
-                    matches = 1
-                } else {
-                    worth = (worth * 2)
-                    matches++
-                }
-            }
+            ourNumbers.add(integer)
         }
 
         val gameIdInteger = gameId.toInt()
 
-        println("matches for $gameIdInteger is $matches")
-        
+        val matchingNumbers = getNumberOfMatches(winningList, ourNumbers)
+
+        listOfCards.add(
+            Card(
+                gameIdInteger,
+                matchingNumbers
+            )
+        )
     }
 
-    println("Total extra scratchers: $totalCards")
+    var i = 0
+    var j = listOfCards.size
+
+    while (i < j)
+    {
+        val card = listOfCards[i]
+        for (k in 1..card.matches)
+        {
+            listOfCards.add(listOfCards[card.number + k - 1])
+        }
+        i++
+        j = listOfCards.size
+    }
+
+    println(listOfCards.size)
 }
+
+data class Card(
+    val number: Int,
+    val matches: Int
+)
+
+fun getNumberOfMatches(winningNumbers: List<Int>, myNumbers: List<Int>): Int
+{
+    var matches = 0
+    val map = HashMap<Int, Int>()
+
+    for (number in myNumbers)
+    {
+        map[number] = map.getOrDefault(number, 0) + 1
+    }
+
+    for (number in winningNumbers)
+    {
+        if (map.getOrDefault(number, 0) > 0)
+        {
+            matches++
+            map[number] = map.getValue(number) - 1
+        }
+    }
+
+    return matches
+}
+
